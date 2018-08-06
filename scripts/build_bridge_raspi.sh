d07688260644230a5b53f9a2f44d307f619084f0
jmalena <jonas.malena@gmail.com>
2018-08-06 17:00:56 +0200

    rename "bridge" folder to "cbits" (follow convention)

--- scripts/build_bridge_raspi.sh
+++ scripts/build_bridge_raspi.sh
@@ -4,5 +4,5 @@ set -e
 
 DIR=${BASH_SOURCE%/*}
 
-g++ -c ${DIR}/../bridge/bridge.cpp -o ${DIR}/../bridge/bridge.o -I${DIR}/../deps/clibspi/include -I${DIR}/../deps/clibcdc/include -I${DIR}/../deps/clibdpa/include -I${DIR}/../deps/clibdpa/Dpa -I${DIR}/../deps/clibdpa/IqrfSpiChannel -I${DIR}/../deps/clibdpa/IqrfCdcChannel
-ar rvs ${DIR}/../bridge/libbridge.a ${DIR}/../bridge/bridge.o
+g++ -c ${DIR}/../cbits/bridge.cpp -o ${DIR}/../cbits/bridge.o -I${DIR}/../deps/clibspi/include -I${DIR}/../deps/clibcdc/include -I${DIR}/../deps/clibdpa/include -I${DIR}/../deps/clibdpa/Dpa -I${DIR}/../deps/clibdpa/IqrfSpiChannel -I${DIR}/../deps/clibdpa/IqrfCdcChannel
+ar rvs ${DIR}/../cbits/libbridge.a ${DIR}/../cbits/bridge.o
