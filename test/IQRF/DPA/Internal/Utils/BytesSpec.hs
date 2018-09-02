be7d220748af0cc0dba97d610de6f4334ba9ba29
jmalena <jonas.malena@gmail.com>
2018-09-02 21:02:13 +0200

    add missing tests for IQRF.DPA.Internal.Utils.Bytes

--- test/IQRF/DPA/Internal/Utils/BytesSpec.hs
+++ test/IQRF/DPA/Internal/Utils/BytesSpec.hs
@@ -7,6 +7,14 @@ import IQRF.DPA.Internal.Utils.Bytes
 
 spec :: Spec
 spec = do
+  describe "packWord16" $ do
+    it "should pack two Word8 into Word16" $ do
+      packWord16 0xAA 0xBB @?= 0xAABB
+
+  describe "unpackWord16LE" $ do
+    it "should unpack Word16 into two Word8 in the little-endian order" $ do
+      unpackWord16LE 0xAABB @?= (0xBB, 0xAA)
+
   describe "bitMap" $ do
     it "should always return list of length n (eventually right padded with zeros)" $ do
       bitSet 0 [0] @?= []
